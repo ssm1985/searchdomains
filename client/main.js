1361cb1e644d511b8599eeb545110ba088bb3524
Spencer McLeod <spencer.mcleod@edu.sait.ca>
2018-04-25 21:29:12 -0400

    almost there

--- client/main.js
+++ client/main.js
@@ -14,17 +14,49 @@ Template.body.helpers({
 
 Template.edit.helpers({
   updateDomain(domainId){
-    // console.log(Domains.findOne({_id: domainId}));
-    console.log('Session.get---- ', Session.get('domainId'));
     const domain = Session.get('domainId');
     return Domains.findOne({_id: domain});
   }
 });
 Template.edit.events({
   'submit .edit-form': function(){
-    // Meteor.call('domains.update', domain);
-    // Close modal
-    console.log('Event', event);
+    event.preventDefault();
+    const target = event.target;
+    const domain = target.text;
+    let tld = "";
+    let tldFlag = false;
+
+    // Convert all strings to lower case except the domain itself
+    for (i = 2; i < domain.length; i++) {
+      domain[i].value = domain[i].value.toLowerCase();
+    }
+    // Extract the TLD
+    for (i = 0; i < domain[0].value.length; i++) {
+      if (domain[0].value[i] ===".") {
+        tldFlag = true;
+      }
+      if (tldFlag) {
+        tld = tld.concat(domain[0].value[i]);
+      }
+      if (i === domain[0].value.length - 1) tldFlag = false;
+    }
+    // Load all form fields into object and send to DB
+    const domainFields = {
+      domain: domain[0].value,
+      dateCreated: domain[1].value,
+      keywords: [
+        domain[2].value,
+        domain[3].value,
+        domain[4].value,
+        domain[5].value,
+        domain[6].value,
+        domain[7].value,
+        domain[8].value,
+        domain[9].value
+      ],
+      tld: tld,
+    };
+    Meteor.call('domains.update', domainFields);
     $('#editModal').modal('close');
     return false;
   },
@@ -89,7 +121,6 @@ Template.domain.events({
     event.preventDefault();
     // return Domains.findOne({_id: this._id});
     Session.set('domainId', this._id);
-    console.log(Session.get('domainId'));
   }
 });
 
