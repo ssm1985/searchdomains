96b2f99c1cb83ce629aba3e095ee99918eb00ce9
Spencer <spencer.s.mcleod@gmail.com>
2018-05-24 11:58:13 -0600

    game on

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
+import { Mongo } from 'meteor/mongo';
 
 Meteor.startup(() => {
   // code to run on server at startup
